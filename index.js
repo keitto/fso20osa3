15173c4d5b18d3e4f3071fa69ed43b1b20f113e3
Teemu Keränen <teemu.keranen@me.com>
2020-08-06 16:37:21 +0300

    3.4 delete, some clogs

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const port = 3001
 
 app.use(express.json())
 
-const persons = [
+const rootPage = `<h1>Hello</h1>
+<p>Try the following:</p>
+<ul>
+<li><a href="/info">info page</a></li>
+<li><a href="/api/persons">persons api</a></li>
+</ul>`
+
+let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -23,11 +30,40 @@ const persons = [
     ]
 
 app.get('/',(req,res) => {
-    res.send('<h1>hello</h1>')
+    console.log("/", new Date())
+    res.send(rootPage)
+})
+app.get('/info', (req,res) => {
+    console.log("/info", new Date())
+    const entries = persons.length
+    res.send(`<p>Phonebook has ${entries} entries</p>
+        <p>${new Date()}</p>`)
 })
 
 app.get('/api/persons', (req,res) => {
+    console.log("/api get all", new Date())
     res.send(persons)
+})
+app.get('/api/persons/:id', (req,res) => {
+    console.log("/api get one", new Date())
+    const id = Number(req.params.id)
+    const person = persons.find(n => n.id === id)
+    if(person) {
+        res.send(person)
+    } else {
+        res.status(404).end()
+    }
+})
+app.delete('/api/persons/:id', (req,res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(n => n.id === id)
+    console.log("/api delete", id, new Date())
+    if(person) {
+        persons = persons.filter(n => n.id !== id)
+        res.status(204).end()
+    } else {
+        res.status(204).end() // iffing in case we want to say 404
+    }
 })
 
 app.listen(port, () => {
