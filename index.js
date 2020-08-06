1d719bc1ff653bdd5dd7cd65e0c3424a9018d467
Teemu Keränen <teemu.keranen@me.com>
2020-08-06 15:45:07 +0300

    json api

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ const port = 3001
 
 app.use(express.json())
 
+const persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+    ]
+
 app.get('/',(req,res) => {
     res.send('<h1>hello</h1>')
+})
+
+app.get('/api/persons', (req,res) => {
+    res.send(persons)
 })
 
 app.listen(port, () => {
